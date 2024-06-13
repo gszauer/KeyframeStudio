2ab98ea3c0582e6b58f62bd09bb60f2256b5782f
gszauer <git@gabormakesgames.com>
2024-06-12 18:13:06 -0700

    Working on color picker

--- js/UIColorPicker.js
+++ js/UIColorPicker.js
@@ -19,8 +19,12 @@ export default class UIColorPicker {
     _hueTrack = null;
     _hueSprites = null;
 
+    _currentHsv = null;
+
     constructor(scene) {
         this._scene = scene; 
+
+        this._currentHsv = new ColorRGB(0.8, 0.2, 0.2).hsv;
 
         this._borderSprite = scene.add.sprite(0, 0, UIGlobals.Atlas, UIGlobals.Solid);
         this._borderSprite.setDepth(UIGlobals.OverlayLayer);
@@ -43,6 +47,14 @@ export default class UIColorPicker {
             sprite.setDepth(UIGlobals.OverlayLayer);
             sprite.setOrigin(0, 0);
             this._hueSprites.push(sprite);
+        }
+
+        this._gradientSprites = [];
+        for (let i = 0; i < 5 * 5; ++i) {
+            const sprite = scene.add.sprite(0, 0, UIGlobals.Atlas, UIGlobals.Solid);
+            sprite.setDepth(UIGlobals.OverlayLayer);
+            sprite.setOrigin(0, 0);
+            this._gradientSprites.push(sprite);
         }
     }
 
@@ -103,6 +115,46 @@ export default class UIColorPicker {
                 rgbTop.color, rgbTop.color, 
                 rgbBottom.color, rgbBottom.color);
         }
+
+        x += hueTrackWidth;
+        x += margin;
+        width -= hueTrackWidth;
+        width -= margin;
+
+        const topLeft = new ColorHSV(this._currentHsv.h, 0, 1).rgb;
+        const topRight = new ColorHSV(this._currentHsv.h, 1, 1).rgb;
+        const bottomLeft = new ColorHSV(this._currentHsv.h, 0, 0).rgb;
+        const bottomRight = new ColorHSV(this._currentHsv.h, 1, 0).rgb;
+
+        const pickAreaWidth = height;
+        const pickAreHeight = height;
+        const slice_w = Math.floor(pickAreaWidth / 5.0);
+        const slice_h = Math.floor(pickAreHeight / 5.0);
+
+        for (let col = 0; col < 5; ++col) {
+            const x0 = Math.floor(col / 5.0);
+            const x1 = Math.floor((col + 1) / 5.0);
+
+            const topInterpolated = topLeft.lerp(topRight, x0)
+
+            for (let row = 0; row < 5; ++row) {
+                const sprite = this._gradientSprites[col + row * 5];
+
+                const y0 = Math.floor(row / 5.0);
+                const y1 = Math.floor((row + 1) / 5.0);
+
+                const _tl = topLeft.lerp(topRight, x0).lerp(bottomLeft.lerp(bottomRight, x0), y0).color;
+                const _tr = topLeft.lerp(topRight, x1).lerp(bottomLeft.lerp(bottomRight, x1), y0).color;
+
+                const _bl = topLeft.lerp(topRight, x0).lerp(bottomLeft.lerp(bottomRight, x0), y1).color;
+                const _br = topLeft.lerp(topRight, x1).lerp(bottomLeft.lerp(bottomRight, x1), y1).color;
+
+                sprite.setPosition(x + col * slice_w, y + row * slice_h);
+                sprite.setScale(slice_w, slice_h);
+                //sprite.setTintFill(topLeft.color, topRight.color, bottomLeft.color, bottomRight.color);
+                sprite.setTintFill(_tl, _tr, _bl, _br);
+            }
+        }
     }
 
     SetVisibility(visible) {
@@ -111,6 +163,9 @@ export default class UIColorPicker {
         this._hueTrack.setActive(visible).setVisible(visible);
         for (let i = 0; i < 10; ++i) {
             this._hueSprites[i].setActive(visible).setVisible(visible);
+        }
+        for (let i = 0; i < 5 * 5; ++i) {
+            this._gradientSprites[i].setActive(visible).setVisible(visible);
         }
     }
 
