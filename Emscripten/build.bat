f5191c51ae24b0a490dcf124ba3ef86e89e5d917
Gabor Szauer <git@gabormakesgames.com>
2024-12-02 02:03:39 -0800

    Trying to turn into PWA

--- Emscripten/build.bat
+++ Emscripten/build.bat
@@ -1,1 +1,3 @@
+CP manifest.json ./Build/manifest.json
+
 emcc build.cpp -g -o Build/index.html -fno-rtti -fno-exceptions -sUSE_WEBGL2 --shell-file=shell.html -s ALLOW_MEMORY_GROWTH=1 -s INITIAL_MEMORY=134217728 -Wno-nontrivial-memaccess -Wno-format-security -Wno-dynamic-class-memaccess --js-library PlatformWasm.js -sEXPORTED_RUNTIME_METHODS=setValue -s EXPORTED_FUNCTIONS="['_main', '_malloc', '_free', '_WASM_InvokePlatformSelectCallback']"
